27ffeae185f1c924eeb65619b3a073f125449089
Trubachev Alex <trubachev@Trubachevs-Mini.Dlink>
2016-04-14 05:26:43 +0300

    Add prefix

---
+++ app/controllers/api/home.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    router = express.Router(),
+    mongoose = require('mongoose'),
+    User = mongoose.model('User');
+
+router.use(function(req, res, next) {
+    next();
+});
+
+module.exports = function (app, prefix) {
+	router.get('/', function (req, res, next) {
+		res.send({message : "Welcome to our API"});
+	});
+	app.use(prefix, router);
+};
